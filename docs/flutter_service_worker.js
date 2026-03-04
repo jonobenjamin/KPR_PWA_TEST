5825b892e44a876502cce9a583753663837cdb01
jonobenjamin <163100744+jonobenjamin@users.noreply.github.com>
2026-03-04 08:19:39 +0200

    Update flutter_service_worker.js

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -166,8 +166,21 @@ self.addEventListener("fetch", (event) => {
     return;
   }
   // If the URL is the index.html, perform an online-first request.
+  // if (key == '/') {
+  //   return onlineFirst(event);
+  // }
   if (key == '/') {
-    return onlineFirst(event);
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+  return;
   }
   event.respondWith(caches.open(CACHE_NAME)
     .then((cache) =>  {
