d2a56d505d5d6b21a186f721b0caa2c675979325
jonobenjamin <163100744+jonobenjamin@users.noreply.github.com>
2026-05-28 09:59:45 +0200

    Add files via upload

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "058813e6162724bf1c0f3068dd0bc297",
-"version.json": "f51a04e4ea500b95343b0740140db670",
+const RESOURCES = {"flutter_bootstrap.js": "f002a743e5135c12368b7e95749f254b",
+"version.json": "043bf1ba96c91615f447e45141ee25de",
 "index.html": "cbb956f6720a915b5fedf77b41245137",
 "/": "cbb956f6720a915b5fedf77b41245137",
-"main.dart.js": "2f8447446509105e1dc7e33a7ac13a3b",
+"main.dart.js": "8b47310c261b6ef3b9d00ed67f6ee0c0",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "0658615ef1bdea8a662d5bb1c68d97b6",
@@ -24,7 +24,7 @@ const RESOURCES = {"flutter_bootstrap.js": "058813e6162724bf1c0f3068dd0bc297",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "fed58b285154c96f3caf10a67c7cd72e",
-"assets/fonts/MaterialIcons-Regular.otf": "662463b7e278ac1b2d533c5cc034862d",
+"assets/fonts/MaterialIcons-Regular.otf": "2f426446a93895036486741b87fc235c",
 "assets/assets/Consession_boundary.geojson": "7f04e0566b20c6e8e96feca2a9a4031e",
 "assets/assets/images/KPR_logo.png": "f70391debeb086a102e3f8fe1a447937",
 "assets/assets/images/KPR_PWA_Background_image.png": "72e974e963f2e43a63b6b259b330fbae",
