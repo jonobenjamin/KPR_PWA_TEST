f4958893e520dafbe432463c13ceebe54722727a
jonobenjamin <163100744+jonobenjamin@users.noreply.github.com>
2026-06-01 11:51:57 +0200

    Add files via upload

--- docs/auth-controller.js
+++ docs/auth-controller.js
@@ -79,7 +79,7 @@ class AuthController {
     if (window._flutter && window._flutter.loader) {
       const loadPromise = window._flutter.loader.load({
         serviceWorkerSettings: {
-          serviceWorkerVersion: "4097741482"
+          serviceWorkerVersion: "2111050379"
         }
       });
       if (loadPromise && typeof loadPromise.then === 'function') {
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "eda051017cc803d4cba5b428c62c7b49",
+const RESOURCES = {"flutter_bootstrap.js": "7aadff928ebcd4f12cb5e47b47ae3fa0",
 "version.json": "954e0901788d4c159b41e9c4f779f3f5",
 "index.html": "cbb956f6720a915b5fedf77b41245137",
 "/": "cbb956f6720a915b5fedf77b41245137",
-"main.dart.js": "a22fc8155d6aa839bf43a909ae924ca6",
+"main.dart.js": "4f521c418975c12f55af9a6c453027d0",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "0658615ef1bdea8a662d5bb1c68d97b6",
@@ -24,7 +24,7 @@ const RESOURCES = {"flutter_bootstrap.js": "eda051017cc803d4cba5b428c62c7b49",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "7b1dda69a587a68016a1874a907b05c3",
-"assets/fonts/MaterialIcons-Regular.otf": "c76c8bb31959620588ce36159fc8695e",
+"assets/fonts/MaterialIcons-Regular.otf": "2c09c8d4ff02689375036ae9762a7e46",
 "assets/assets/Consession_boundary.geojson": "7f04e0566b20c6e8e96feca2a9a4031e",
 "assets/assets/images/KPR_logo.png": "f70391debeb086a102e3f8fe1a447937",
 "assets/assets/images/KPR_PWA_Background_image.png": "72e974e963f2e43a63b6b259b330fbae",
